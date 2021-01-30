7afe8d8205abc63e01306cc39afba545cddeb9ef
nick@deploy.at <mikowhy@tutanota.com>
2021-01-30 11:43:41 +0100

    feat: generate number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number is {}", secret_number);
+
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
